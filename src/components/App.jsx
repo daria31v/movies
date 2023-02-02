5b962f78e2e82ed74dab5baa1caabfae22182680
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2023-02-03 00:32:13 +0100

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+// import { Routes, Route } from "react-router-dom";
+// import Home from "path/to/pages/Home";
+// import About from "path/to/pages/About";
+// import Products from "path/to/pages/Products";
+
+// export const App = () => {
+//   return (
+//     <div>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/products" element={<Products />} />
+          // <Route path="*" element={<NotFound />} />
+//       </Routes>
+//     </div>
+// <nav>
+//   <Link to="/">Home</Link>
+//   <Link to="/about">About</Link>
+//   <Link to="/products">Products</Link>
+// </nav>
+//   );
+// };
