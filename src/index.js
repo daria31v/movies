08d45040c895ec0da7693feb2cf4b120d486e7be
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2023-07-02 11:15:32 +0200

    some changes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Movies">
+    <BrowserRouter basename="/movies/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
